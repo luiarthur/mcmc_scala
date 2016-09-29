73e71d80d881e6ed592df5bba8fd0a5e121f12a2
Arthur Lui <luiarthur@gmail.com>
2016-09-28 21:01:10 -0700

    yes

--- src/main/scala/MCMC/all/FullCond.scala
+++ src/main/scala/MCMC/all/FullCond.scala
@@ -1,5 +1,5 @@
 package MCMC.all
 
 case class FullCond (val sampler: Vector[Double] => Double) {
-  def rand(otherParams: Vector[Double]) = sampler(otherParams)
+  def rand(params: Vector[Double]) = sampler(params)
 }
