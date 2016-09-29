b52de68510364127e1c1516ea08ab6e824dc6fed
Arthur Lui <luiarthur@gmail.com>
2016-09-29 10:44:24 -0700

    yes

--- src/main/scala/MCMC/all/State.scala
+++ src/main/scala/MCMC/all/State.scala
@@ -1,3 +1,16 @@
 package MCMC.all
 
-case class State(s: Map[String, Param])
+case class State(s: Map[String, Param]) {
+
+  override def toString = {
+    def str(v: Param) = v.size match {
+      case 1 => v.head
+      case _ => v
+    }
+
+    val l = s.toList
+    val out = l map { pv => pv._1 + ": " + str(pv._2) }
+    out.mkString(";\t")
+  }
+
+}
