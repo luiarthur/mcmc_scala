0c6fda900a108b2c1d675441f5c81fd9e0726f16
Arthur Lui <luiarthur@gmail.com>
2016-10-02 11:30:00 -0700

    yes

--- src/main/scala/MCMC/all/Gibbs.scala
+++ src/main/scala/MCMC/all/Gibbs.scala
@@ -14,7 +14,19 @@ object Gibbs {
         S
       }
     }
-    loop(List(init),0).asInstanceOf[List[T]]
+    loop(List(init),0).take(B).asInstanceOf[List[T]]
+    /* Non-recursive (preallocate memory)
+     * while-loops avoid creating objects (whereas for loops do not)
+    val out = Array.fill[State](B+burn)(init)
+    var i = 1
+    while (i < B+burn) {
+      if (i % ((B+burn)/10) == 0) print("\rProgress: " + i +"/"+ (B+burn))
+      out(i) = out(i-1).update
+      i += 1
+    }
+    print("\rProgress: " + i + "/" + (B + burn) + "\n")
+    out.drop(burn).asInstanceOf[Array[T]].toList
+    */
   }
 
 }
