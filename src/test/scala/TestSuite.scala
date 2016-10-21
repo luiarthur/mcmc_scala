eefb0b949ee89474dfe5abb2926de6cc57691719
Arthur Lui <luiarthur@gmail.com>
2016-10-20 20:38:11 -0700

    split AFT to another project

--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -43,51 +43,9 @@ class TestSuite extends FunSuite {
     val out = timer {Gibbs.sample(init=State(mu=2.0,sig2=10.0),B=10000,burn=1000)}
     out.take(3).foreach{println}
 
-
     println("post mean mu: " + mean(out.map{_.mu}) )
     println("post mean sig2: " + mean(out.map{_.sig2}) )
     println("post sd mu: " + sd(out.map{_.mu}) )
     println("post sd sig2: " + sd(out.map{_.sig2}) )
-  }
-
-  test("aft") {
-    val XTV = scala.io.Source.fromFile("src/test/resources/tongue.dat").getLines.map(x=>x.split(",").toList.map(_.toDouble)).toList
-    val X = XTV.map(xtv => List(1.0,xtv(0)))
-    val t = XTV.map(xtv => xtv(1))
-    val v = XTV.map(xtv => xtv(2).toInt)
-
-    import MCMC.all._
-    Rand.reSeed(1L)
-
-    val B = 10000
-    val burn = 5000
-
-    val prior = new AFT.Prior(m = List(0,0), s2 = List(10,10), csb = List(1,1),
-                              a = 2.0, b = 1.0, css = 1)
-    val aft = new AFT(t,X,v,prior)
-
-    val (weib,wdic) = timer{aft.sample(B,burn)}
-    weib.take(2).foreach{println}
-    println(Console.GREEN+"weib: "+weib.map(_.sig).sum/B+Console.RESET)
-    println(Console.GREEN+"weib: "+sd(weib.map(_.sig))+Console.RESET)
-    println(Console.GREEN+"weib: "+weib.map(_.beta(0)).sum/B+Console.RESET)
-    println(Console.GREEN+"weib: "+weib.map(_.beta(1)).sum/B+Console.RESET)
-    println(Console.GREEN+"weib: "+sd(weib.map(_.beta(0)))+Console.RESET)
-    println(Console.GREEN+"weib: "+sd(weib.map(_.beta(1)))+Console.RESET)
-    println(Console.GREEN+"weib DIC: "+wdic+Console.RESET)
-
-    val (llog,lldic) = timer{aft.sample(B,burn,distr="LogLogistic")}
-    println(Console.GREEN+"llog: "+llog.map(_.sig).sum/B+Console.RESET)
-    println(Console.GREEN+"llog: "+sd(llog.map(_.sig))+Console.RESET)
-    println(Console.GREEN+"llog: "+llog.map(_.beta(0)).sum/B+Console.RESET)
-    println(Console.GREEN+"llog: "+llog.map(_.beta(1)).sum/B+Console.RESET)
-    println(Console.GREEN+"llog DIC: "+lldic+Console.RESET)
-
-    val (lnorm,lndic) = timer{aft.sample(B,burn,distr="LogNormal")}
-    println(Console.GREEN+"lnorm: "+lnorm.map(_.sig).sum/B+Console.RESET)
-    println(Console.GREEN+"lnorm: "+sd(lnorm.map(_.sig))+Console.RESET)
-    println(Console.GREEN+"lnorm: "+lnorm.map(_.beta(0)).sum/B+Console.RESET)
-    println(Console.GREEN+"lnorm: "+lnorm.map(_.beta(1)).sum/B+Console.RESET)
-    println(Console.GREEN+"lnorm DIC: "+lndic+Console.RESET)
   }
 }
